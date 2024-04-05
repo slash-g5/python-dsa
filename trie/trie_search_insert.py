70502bf2c1cebff7f463a2be34d2f7b64f1817c7
slash-g5 <1729shashank@gmail.com>
2024-04-05 18:14:47 +0530

    Trie Search And insert

---
+++ trie/trie_search_insert.py
@@ -0,0 +1,42 @@
+class TrieNode:
+
+    def __init__(self):
+        self.children = [None] * 26
+
+        # isEndOfWord is True if node represent the end of the word
+        self.isEndOfWord = False
+
+class Solution:
+    # Function to insert string into TRIE.
+    def insert(self, root, key):
+        # code here
+        if len(key) == 0:
+            return
+
+        if root is None:
+            return
+
+        temp = root
+
+        for i in range(0, len(key)):
+            if temp.children[ord(key[i]) - ord('a')] is None:
+                temp.children[ord(key[i]) - ord('a')] = TrieNode()
+            temp = temp.children[ord(key[i]) - ord('a')]
+
+        temp.isEndOfWord = True
+
+    # Function to use TRIE data structure and search the given string.
+    def search(self, root, key):
+
+        # code here
+        if len(key) == 0:
+            return 0
+
+        temp = root
+
+        for i in range(len(key)):
+            if temp.children[ord(key[i]) - ord('a')] is None:
+                return 0
+            temp = temp.children[ord(key[i]) - ord('a')]
+
+        return temp.isEndOfWord
