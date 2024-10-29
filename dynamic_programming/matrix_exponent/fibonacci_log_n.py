69acab517070a8f4c6ba130faf3664619e515a8c
slash-g5 <1729shashank@gmail.com>
2024-10-29 16:58:44 +0530

    computing fibonacci in log n time

---
+++ dynamic_programming/matrix_exponent/fibonacci_log_n.py
@@ -0,0 +1,39 @@
+# calculate fibonacci in log(n)
+
+def fibonacci(n):
+    # calculate M**p
+
+    # multiply two matrix
+    def multiply(M1, M2):
+        res = [[0 for _ in range(len(M2[0]))] for _ in range(len(M1))]
+        for i in range(len(M1)):
+            for j in range(len(M2[0])):
+                res[i][j] = 0
+                for k in range(len(M1[0])):
+                    res[i][j] += M1[i][k] * M2[k][j]
+        return res
+
+    def mat_expo(M, p):
+        if p == 0:
+            return [[1, 1], [1, 1]]
+        if p == 1:
+            return M
+        if p == 2:
+            return multiply(M, M)
+        if p % 2 == 0:
+            return mat_expo(multiply(M, M), p // 2)
+        return multiply(M, mat_expo(M, p - 1))
+
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    B = [[1, 1], [1, 0]]
+    A2 = [[1], [0]]
+
+    return sum(multiply(mat_expo(B, n-2), A2)[0])
+
+
+if __name__ == "__main__":
+    print(fibonacci(66))
